f5e1cc815c966404cc74bdb869258a8ceead6e93
SeamlyUK <138593701+SeamlyUK@users.noreply.github.com>
2025-05-29 15:28:47 +0100

    Add files via upload

--- counter.js
+++ counter.js
@@ -1,40 +1,42 @@
 
 let screenOne = document.getElementById("screen1")
 let screenTwo = document.getElementById("screen2")
-let count = 0
+let lcount = 0
+let rcount = 0 
 
 function ladd1() {
-    count += 1
-    screenOne.textContent = count
+    lcount += 1
+    screenOne.textContent = lcount
 }
 
 function ladd2() {
-    count += 2
-    screenOne.textContent = count
+    lcount += 2
+    screenOne.textContent = lcount
 }
 
 function ladd3() {
-    count += 3
-    screenOne.textContent = count
+    lcount += 3
+    screenOne.textContent = lcount
 }
 
 function radd1() {
-    count += 1
-    screenTwo.textContent = count
+    lcount += 1
+    screenTwo.textContent = rcount
 }
 
 function radd2() {
-    count += 2
-    screenTwo.textContent = count
+    rcount += 2
+    screenTwo.textContent = rcount
 }
 
 function radd3() {
-    count += 3
-    screenTwo.textContent = count
+    rcount += 3
+    screenTwo.textContent = rcount
 }
 
 function reset() {
-    count = 0
+    rcount = 0
+    lcount = 0
     screenTwo.textContent = 0
     screenOne.textContent = 0
 }

057c38df673c4b1e809481a68a63697bc2f33791
SeamlyUK <Nabaz.javonhari@gmail.com>
2025-06-03 16:06:50 +0100

    New project

---
+++ Random Number/random.js
@@ -0,0 +1,14 @@
+let fromNum = document.getElementById("from")
+let toNum = document.getElementById("to")
+let minNum = 0
+let maxNum = 0
+
+
+
+function generate() {
+
+    // get numbers from input 
+
+minNum = fromNum.textContent.valueOf
+    
+}

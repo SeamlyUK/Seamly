b33fcdb8170c43c6a78c68549bab593e2e971379
SeamlyUK <Nabaz.javonhari@gmail.com>
2025-06-04 10:38:17 +0100

    Random number

--- Random Number/random.js
+++ Random Number/random.js
@@ -1,5 +1,5 @@
-let fromNum = document.getElementById("from")
-let toNum = document.getElementById("to")
+
+let screenNum = document.getElementById("screen")
 let minNum = 0
 let maxNum = 0
 
@@ -8,7 +8,24 @@ let maxNum = 0
 function generate() {
 
     // get numbers from input 
-
-minNum = fromNum.textContent.valueOf
+let fromNum = document.getElementById("from").value
+let toNum = document.getElementById("to").value
+console.log(fromNum)
+console.log(toNum)
+    // generate rando number between range
+let ranNum = Math.floor(Math.random() * (toNum - fromNum + 1)) 
+    console.log(ranNum)
     
+    // make the screen show random number
+screenNum.textContent = ranNum
+
+if (fromNum > toNum) {
+screenNum.textContent = "Your min range number can't be higher than your max range number"
+document.getElementById("screen").style.color = "red"    
+  
+} else if (fromNum === toNum) {
+screenNum.textContent = "Your range numbers are the same"
+document.getElementById("screen").style.color = "red"    
+
+}
 }

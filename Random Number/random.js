2f47c2e56d17626e2ca167ca6fb3331263ac34c7
SeamlyUK <Nabaz.javonhari@gmail.com>
2025-06-04 13:16:41 +0100

    Fix of random number generator

--- Random Number/random.js
+++ Random Number/random.js
@@ -1,24 +1,25 @@
 
 let screenNum = document.getElementById("screen")
-let minNum = 0
-let maxNum = 0
-
 
 
 function generate() {
 
     // get numbers from input 
-let fromNum = document.getElementById("from").value
-let toNum = document.getElementById("to").value
-console.log(fromNum)
-console.log(toNum)
+let fromNum = Number(document.getElementById("from").value)
+let toNum = Number(document.getElementById("to").value)
+
+console.log(fromNum + "from")
+console.log(toNum + "to")
     // generate rando number between range
-let ranNum = Math.floor(Math.random() * (toNum - fromNum + 1)) 
+
+
+    let ranNum = Math.floor(Math.random() * ( toNum - fromNum + 1) + fromNum) 
     console.log(ranNum)
     
     // make the screen show random number
 screenNum.textContent = ranNum
 
+    // data validation
 if (fromNum > toNum) {
 screenNum.textContent = "Your min range number can't be higher than your max range number"
 document.getElementById("screen").style.color = "red"    
